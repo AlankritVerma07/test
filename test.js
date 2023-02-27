b197bb2dab583d1aed938174b491ca2b8f7f3697
AlankritVerma07 <alankritverma.lko@gmail.com>
2023-02-27 15:06:47 +0530

    Added IIFE

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-const el=document.getElementById("buttoons")
+(function(){
+    console.log("hello");
+    const el=document.getElementById("buttons")
+    var aTag=document.createElement('a')
+    aTag.setAttribute('href',"yourLink")
+    aTag.textContent="Link Text"
+    el.appendChild(aTag)
+})() 
